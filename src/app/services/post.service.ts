5a4202039b24b260e759eb9d3518aa1d84e90b29
AARON BURNS - STUDENT <G00346688@gmit.ie>
2018-11-28 13:52:38 +0000

    added dice code have to change to suit typescript

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -55,5 +55,30 @@ export class PostService {
     var smallestNum=9999
 
     console.log(roll1 + " " + roll2);
+
+    /*
+    int stats[]= new int[6];
+		int rolls[] = new int[4];
+		int i, j;
+		int num1, num2, num3;
+		int smallestNum=9999;
+		int statSum=0;
+	  	
+	
+			  for(i=0;i<4;i++)
+			  {
+				  rolls[i]= (int) (Math.random()*6+1);
+				  if(rolls[i] < smallestNum)
+					  smallestNum= rolls[i];	  
+			  }
+	
+			  for(i=0;i<4;i++)
+			  {				  
+				  for(j=0;j<6;j++)
+					  stats[j]+=rolls[i];
+			  }
+			  for(j=0;j<6;j++)
+System.out.println(stats[j]-smallestNum);
+    */
   }
 }
